66ac0c28a1c401c46835c786967629d541ef20d4
daeyoung <pstor23@gmail.com>
2024-12-04 21:24:30 +0900

    Revert "feat: 주문 내역 페이지 구현 ( 이미지 제외 )"
    This reverts commit f6999b0257aeed8717c91b3ebe4222d8cbba2b96.

--- src/main/java/org/example/coffee/controller/OrderController.java
+++ src/main/java/org/example/coffee/controller/OrderController.java
@@ -1,10 +1,5 @@
 package org.example.coffee.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.example.coffee.dao.OrderDAO;
-import org.example.coffee.dto.OrderDTO;
-import org.example.coffee.dto.OrderProductDTO;
-import org.example.coffee.dto.OrderSummaryDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/orders")
-@RequiredArgsConstructor
 public class OrderController {
-
-    private final OrderDAO orderDAO;
 
     @GetMapping
     public String userCheckForm() {
@@ -28,16 +18,6 @@ public class OrderController {
 
     @PostMapping
     public String orderList(@RequestParam String email, Model model) {
-        OrderDTO order = orderDAO.findOrderByEmail(email);
-        if (order == null) {
-            return "redirect:/orders";
-        }
-
-        List<OrderProductDTO> orderProducts = orderDAO.findOrderProductsByEmail(email);
-        model.addAttribute("orderProducts", orderProducts);
-
-        OrderSummaryDTO orderSummary = orderDAO.findOrderSummary(email);
-        model.addAttribute("orderSummary", orderSummary);
 
         return "order_list";
     }
